e5c06a32aa0bb7fa8cc80e9d1b64e5699e29e6af
notasquid1938 <shoebenfilm@gmail.com>
2024-01-09 19:41:27 -0600

    All Posts can now be opened up for any date range using a very ineffecient "look through everything" approach

--- reddit-clone/pages/api/Comments.js
+++ reddit-clone/pages/api/Comments.js
@@ -9,16 +9,22 @@ export default async function handler(req, res) {
     }
 
     const db = await connectToDatabase();
-    const collection = db.collection('RS_2008-12');
+    const collections = await db.listCollections().toArray();
 
-    // Find the post with the specified ID
-    const post = await collection.findOne({ id: id });
+    // Iterate through RS collections and find the post with the specified ID
+    for (const collectionInfo of collections) {
+      if (collectionInfo.name.startsWith('RS')) {
+        const collection = db.collection(collectionInfo.name);
+        const post = await collection.findOne({ id: id });
 
-    if (!post) {
-      return res.status(404).json({ error: 'Post not found' });
+        if (post) {
+          return res.status(200).json(post);
+        }
+      }
     }
 
-    res.status(200).json(post);
+    // If no matching post is found in any collection
+    return res.status(404).json({ error: 'Post not found' });
   } catch (error) {
     console.error('Error fetching post data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
