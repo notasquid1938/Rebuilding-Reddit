876bff45f165980bb057be11ce6c4f9027eede16
notasquid1938 <shoebenfilm@gmail.com>
2024-01-09 20:53:22 -0600

    Changed default start date to make testing easier

--- reddit-clone/pages/index.js
+++ reddit-clone/pages/index.js
@@ -5,7 +5,7 @@ import Timeline from '@/components/Timeline';
 import Submissions from '../components/Submissions';
 
 function HomePage() {
-  const [dateRange, setDateRange] = useState({ startDate: '2005-06', endDate: '2005-06' });
+  const [dateRange, setDateRange] = useState({ startDate: '2008-12', endDate: '2008-12' });
 
   const handleDateRangeChange = (startDate, endDate) => {
     setDateRange({ startDate, endDate });
