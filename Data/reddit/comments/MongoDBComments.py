71d2ae80702f79d0f989e6788ca519030eb100b0
notasquid1938 <shoebenfilm@gmail.com>
2024-01-11 03:32:09 -0600

    Fixed Submission Databse Creation Process, index needs to be for id not link_id and started working on comment database creation

--- Data/reddit/comments/MongoDBComments.py
+++ Data/reddit/comments/MongoDBComments.py
@@ -1,6 +1,7 @@
 import os
 import json
 from pymongo import MongoClient
+from pymongo.errors import OperationFailure
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
@@ -35,10 +36,25 @@ for collection_name in collection_names:
     if collection_name.startswith('RC'):
         collection = db[collection_name]
 
-        # Create an ascending index for the 'id' field
-        collection.create_index([('link_id', 1)])
+        # Check if the index already exists for 'link_id' field
+        try:
+            collection.create_index([('link_id', 1)], name='link_id_index', unique=False)
+            print(f"Ascending index created for 'link_id' field in collection: {collection_name}")
+        except OperationFailure as e:
+            if "Index with name: link_id_index already exists" in str(e):
+                print(f"Ascending index for 'link_id' field already exists in collection: {collection_name}")
+            else:
+                print(f"Error creating index for 'link_id' field in collection {collection_name}: {e}")
 
-        print(f"Index created for 'id' field in collection: {collection_name}")
+        # Check if the index already exists for 'score' field
+        try:
+            collection.create_index([('score', -1)], name='score_index', unique=False)
+            print(f"Descending index created for 'score' field in collection: {collection_name}")
+        except OperationFailure as e:
+            if "Index with name: score_index already exists" in str(e):
+                print(f"Descending index for 'score' field already exists in collection: {collection_name}")
+            else:
+                print(f"Error creating index for 'score' field in collection {collection_name}: {e}")
 
 # Close MongoDB connection
 client.close()
--- Data/reddit/submissions/MongoDBSubmissions.py
+++ Data/reddit/submissions/MongoDBSubmissions.py
@@ -47,8 +47,8 @@ for collection_name in collection_names:
 
         # Check if the index already exists for 'link_id' field
         try:
-            collection.create_index([('link_id', 1)], name='link_id_index', unique=False)
-            print(f"Ascending index created for 'link_id' field in collection: {collection_name}")
+            collection.create_index([('id', 1)], name='id_index', unique=False)
+            print(f"Ascending index created for 'id' field in collection: {collection_name}")
         except OperationFailure as e:
             if "Index with name: link_id_index already exists" in str(e):
                 print(f"Ascending index for 'link_id' field already exists in collection: {collection_name}")
