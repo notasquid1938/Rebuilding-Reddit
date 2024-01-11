15d7d37f436a6c4fd1a28ee521d4ea94a6b6f946
notasquid1938 <shoebenfilm@gmail.com>
2024-01-11 03:34:11 -0600

    Realized Submissions database doesn't need an index for id if its being requested based on score

--- Data/reddit/submissions/MongoDBSubmissions.py
+++ Data/reddit/submissions/MongoDBSubmissions.py
@@ -44,17 +44,7 @@ collection_names = db.list_collection_names()
 for collection_name in collection_names:
     if collection_name.startswith('RS'):
         collection = db[collection_name]
-
-        # Check if the index already exists for 'link_id' field
-        try:
-            collection.create_index([('id', 1)], name='id_index', unique=False)
-            print(f"Ascending index created for 'id' field in collection: {collection_name}")
-        except OperationFailure as e:
-            if "Index with name: link_id_index already exists" in str(e):
-                print(f"Ascending index for 'link_id' field already exists in collection: {collection_name}")
-            else:
-                print(f"Error creating index for 'link_id' field in collection {collection_name}: {e}")
-
+        
         # Check if the index already exists for 'score' field
         try:
             collection.create_index([('score', -1)], name='score_index', unique=False)
