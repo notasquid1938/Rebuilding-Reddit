f455be01c543686d129d02de728129868b68628e
notasquid1938 <shoebenfilm@gmail.com>
2024-01-11 03:53:40 -0600

    Made initial load blank

--- reddit-clone/pages/index.js
+++ reddit-clone/pages/index.js
@@ -5,7 +5,7 @@ import Timeline from '@/components/Timeline';
 import Submissions from '../components/Submissions';
 
 function HomePage() {
-  const [dateRange, setDateRange] = useState({ startDate: '2008-12', endDate: '2008-12' });
+  const [dateRange, setDateRange] = useState({ startDate: '0', endDate: '0' });
 
   const handleDateRangeChange = (startDate, endDate) => {
     setDateRange({ startDate, endDate });
