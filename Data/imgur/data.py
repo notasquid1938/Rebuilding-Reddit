eefa7a72cdaea40889842c8dc5a12674faaaa692
notasquid1938 <shoebenfilm@gmail.com>
2024-01-12 14:52:26 -0600

    Trying to figure out imgur archives, decompressing the zst is proving difficult

---
+++ Data/imgur/data.py
@@ -0,0 +1,6 @@
+##Archives: https://archive.org/details/archiveteam_imgur
+
+##WSL Command:
+##cp /mnt/c/Users/Ben-Shoemaker/Desktop/Coding/Rebuild-Reddit/Data/imgur/imgur_20230509133254_768c86ff.1682559222.megawarc.warc.zst .
+
+##Then, ./zstd.py file.zst
