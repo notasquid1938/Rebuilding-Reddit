2e18e9f9303acba4a1a74d65d4f32ce6e210dd4d
notasquid1938 <shoebenfilm@gmail.com>
2024-01-15 02:38:25 -0600

    API isn't fully ironed out, but meilisearch looks to be a huge improvement. 1.57s to 250ms on search suggestions. I trust that this will scale well when I update the subreddit list.

--- reddit-clone/pages/api/SearchSuggestions.js
+++ reddit-clone/pages/api/SearchSuggestions.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import fuzzysort from 'fuzzysort';
+import { MeiliSearch } from 'meilisearch'
 
 export default async function handler(req, res) {
   const { query } = req.query;
@@ -8,37 +7,15 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Query parameter "query" is required' });
   }
 
-  const startOverall = new Date();
+  const client = new MeiliSearch({
+    host: 'http://127.0.0.1:7700'
+    //apiKey: 'masterKey',
+  })
 
-  // Load subreddits from file
-  const startLoad = new Date();
-  const fileContent = fs.readFileSync('subreddits.txt', 'utf-8');
-  const endLoad = new Date();
-  const loadTime = endLoad - startLoad;
+  const index = client.index('subreddits')
 
-  //console.log(`Time taken for loading subreddits: ${loadTime}ms`);
+  const suggestions = await index.search(query, { limit: 5 });
 
-  // Filter subreddits based on the query prefix
-  const startFilter = new Date();
-  const subreddits = fileContent.split(/\r?\n/).filter(subreddit => subreddit.toLowerCase().startsWith(query.toLowerCase()));
-  const endFilter = new Date();
-  const filterTime = endFilter - startFilter;
-
-  //console.log(`Time taken for filtering subreddits: ${filterTime}ms`);
-
-  // Perform fuzzy search and limit results to top 5
-  const startFuzzySearch = new Date();
-  const fuzzyResults = fuzzysort.go(query.toLowerCase(), subreddits, { limit: 5 });
-  const endFuzzySearch = new Date();
-  const fuzzySearchTime = endFuzzySearch - startFuzzySearch;
-
-  //console.log(`Time taken for fuzzy search: ${fuzzySearchTime}ms`);
-
-  const suggestions = fuzzyResults.map(result => result.target);
-
-  const endOverall = new Date();
-  const totalTime = endOverall - startOverall;
-  //console.log(`Total time taken: ${totalTime}ms`);
 
   res.status(200).json({ suggestions });
 }
