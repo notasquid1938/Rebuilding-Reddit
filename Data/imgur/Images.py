44bd715d3562afc5d28be41859c0e5767f64b96a
notasquid1938 <shoebenfilm@gmail.com>
2024-01-22 10:29:42 -0600

    Modified warc content to pull videos too, works pretty well 15gb of media from 15.5gb warc

--- Data/imgur/Images.py
+++ Data/imgur/Images.py
@@ -9,33 +9,29 @@ def get_filename_from_url(url):
     filename = unquote(parsed_url.path.split("/")[-1])
     return filename
 
-def save_image_from_warc(warc_filename, output_directory):
+def save_media_from_warc(warc_filename, output_directory):
     with open(warc_filename, 'rb') as warc_file:
         for record in warcio.ArchiveIterator(warc_file):
             if record.rec_type == 'response':
                 url = record.rec_headers.get_header('WARC-Target-URI')
                 content_type = record.http_headers.get('Content-Type', '').lower()
 
-                # Check if the response contains an image
-                if 'image' in content_type:
-                    image_data = record.content_stream().read()
+                # Check if the response contains media (image or video)
+                if 'image' in content_type or 'video' in content_type:
+                    media_data = record.content_stream().read()
 
                     # Get the filename from the URL
                     filename = get_filename_from_url(url)
-                    output_image_filename = f"{output_directory}/{filename}"
+                    output_media_filename = f"{output_directory}/{filename}"
 
-                    # Save the image to a file
-                    with open(output_image_filename, 'wb') as output_image_file:
-                        output_image_file.write(image_data)
+                    # Save the media to a file
+                    with open(output_media_filename, 'wb') as output_media_file:
+                        output_media_file.write(media_data)
 
-                    print(f"Image saved from URL: {url} to {output_image_filename}")
-                    break  # Stop after the first image is saved
+                    print(f"Media saved from URL: {url} to {output_media_filename}")
 
 if __name__ == "__main__":
     warc_filename = r"C:\Users\Ben-Shoemaker\Desktop\Coding\Rebuild-Reddit\Data\imgur\imgur_20240106055037_f1cea2ab.1683789516.megawarc.warc"
-    output_directory = "./Data/imgur/images"
+    output_directory = "./Data/imgur/media"
 
-    save_image_from_warc(warc_filename, output_directory)
-
-
-# r"C:\Users\Ben-Shoemaker\Desktop\Coding\Rebuild-Reddit\Data\imgur\imgur_20240106055037_f1cea2ab.1683789516.megawarc.warc"
+    save_media_from_warc(warc_filename, output_directory)
